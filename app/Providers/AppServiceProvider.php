305939a67c367f54080d23df4e62afee38858ef1
jjxuhude <jjxuhuade@163.com>
2018-09-12 21:57:06 +0800

    passport vue
    cnpm install
    php artisan vendor:publish --tag=passport-components
    npm run dev

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,6 +14,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
+        
         defined('DS') || define('DS',DIRECTORY_SEPARATOR );
         $prefix = config('app.backend_prefix');
         if(preg_match('/^\/'.$prefix.'/i', request()->getRequestUri(),$match)){
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Laravel\Passport\Passport;
+use Carbon\Carbon;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -26,5 +28,18 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         //
+        //
+        Passport::routes();
+        Passport::tokensExpireIn((new \DateTime())->add(new \DateInterval('PT1M')));
+        Passport::refreshTokensExpireIn((new \DateTime())->add(new \DateInterval('P1M')));
+        
+        //隐式授权令牌
+        // 隐式授权类似于授权码授权，但是它只令牌将返回给客户端而不交换授权码。这种授权最常用于无法安全存储客户端凭据的 JavaScript 或移动应用程序。通过调用 AuthServiceProvider 中的 enableImplicitGrant 方法来启用这种授权：
+        Passport::enableImplicitGrant();
+        
+        //         Passport::tokensCan([
+        //             'place-orders' => 'Place orders',
+        //             'check-status' => 'Check order status',
+        //         ]);
     }
 }
