60b8befcc6db68a93723a73ca87e542093d5bd20
Xu, Jack, Connext China <Jack.Xu1@connext.com.cn>
2018-09-14 11:43:05 +0800

    db控制器添加vue应用

--- resources/views/frontend/blade.blade.php
+++ resources/views/frontend/blade.blade.php
@@ -1,16 +1,10 @@
 @extends('frontend.layouts.app') @section('content')
-<div id="app1">
-	<span v-for="i in items">${i}</span>
-</div>
 <div class="container">
 	<div style="width: 100%; margin: 0 auto;">
 		<table
 			class="table table-condensed table-hover table table-striped table-bordered">
-			<tr>
-				<th>接口名称</th>
-				<th style="width:50%;">接口描述</th>
-				<th>METHOD</th>
-				<th>参数</th>
+			<tr id="app1">
+				<th v-for="(i,key) in items" :style="getWidth(i)" :key="key">${i}</th>
 			</tr>
 		
 		<?php foreach($methods as $method):?>
@@ -48,10 +42,19 @@ new Vue({
 	delimiters: ['${', '}'],
 	data:function (){
 		return {
-			items:[1,2,3,4,5]
+			items:['接口名称','接口描述','METHOD','参数'],
+			width:{
+				width:"50%"
+			}
+		}
+	},
+	methods:{
+		getWidth:function ($i){
+			if($i=='接口描述'){
+				return 'width:50%;'
+			}
 		}
 	}
-
 })
 </script>
 @endsection
